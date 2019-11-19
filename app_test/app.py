9c3906f8786d0fd52af4b05ce423cfe6fdc2af87
Lan Zhang <zhany016@newschool.edu>
2019-11-19 11:01:23 -0500

    new stuff

--- app_test/app.py
+++ app_test/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, escape, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/baby")
-def hello():
-    return "Hello, World!"
+post = [{
+		'text': "How can a person be honest when they are so afraid of something that is happening to them? I’m not really sure how a person can be honest when they are so afraid of something that is happening to them. It doesn’t exist.” There’s no such thing as fear. There’s no such thing as fear. "
+		},
+		{'text':" love. Save my name, email, and website in this browser for the next time I comment."
+		}]
+
+@app.route('/')
+def index():
+    return render_template('index.html', posts = post);
